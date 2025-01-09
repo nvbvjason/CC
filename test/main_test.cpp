6e99a1852dbd95b22c9fa276a7e01348c391b64f
nvbvjason <jasongadewoltz@gmail.com>
2025-01-09 04:01:47 +0100

    removed unessary tests

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -8,32 +8,6 @@
 
 const std::string testsFolderPath = "/home/jason/src/CC/writing-a-c-compiler-tests/tests/";
 
-TEST(InvalidArguments, notAFile)
-{
-    const std::string validPath = "/home/jason/src/CC/writing-a-c-compiler-tests/tests/chapter_1/";
-    std::vector<std::string> args{"/home/jason/src/CC/cmake-build-debug/CC_run", "", "--codegen"};
-    args[1] = "/home/jason/src/CC/writing-a-c-compiler-tests/tests/ch";
-    const CompilerDriver program(args);
-    ASSERT_NE(0, program.run());
-}
-
-TEST(InvalidArguments, noArgument)
-{
-    const std::string validPath = testsFolderPath + "chapter_1/";
-    constexpr std::vector<std::string> args;
-    const CompilerDriver program(args);
-    ASSERT_NE(0, program.run());
-}
-
-TEST(InvalidArguments, lexing)
-{
-    const std::string validPath = testsFolderPath + "chapter_1/valid/newlines.c";
-    std::vector<std::string> args{"/home/jason/src/CC/cmake-build-debug/CC_run", "--lexing", ""};
-    args[2] = validPath;
-    const CompilerDriver program(args);
-    ASSERT_NE(0, program.run());
-}
-
 TEST(Chpater1, lexingValid)
 {
     const std::string validPath = testsFolderPath + "chapter_1/valid";
@@ -69,23 +43,6 @@ TEST(Chpater1, parsingValid)
         ASSERT_EQ(0, parser.programParse(program)) << path.path();
     }
 }
-
-// TEST(Chpater1, codegenValid)
-// {
-//     const std::string validPath = testsFolderPath + "chapter_1/valid";
-//     for (const auto& path : std::filesystem::directory_iterator(validPath)) {
-//         const std::string sourceCode = getSourceCode(path.path());
-//         std::vector<Lexing::Token> lexemes;
-//         Lexing::Lexer lexer(sourceCode);
-//         ASSERT_EQ(0, lexer.getLexems(lexemes)) << path.path();
-//         Parsing::Parse parser(lexemes);
-//         Parsing::ProgramNode program;
-//         ASSERT_EQ(0, parser.programParse(program)) << path.path();
-//         std::string output;
-//         const Codegen::Assembly astToAssembly(program);
-//         ASSERT_EQ(0, astToAssembly.getOutput(output)) << path.path();
-//     }
-// }
 
 TEST(Chpater1, parsingInvalid)
 {
